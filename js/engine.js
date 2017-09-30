5e68a4c96324898a2984ac7da36bad4f22d8ee30
Arshin Jain <arshinx@users.noreply.github.com>
2017-09-29 23:24:13 -0400

    Load images

--- js/engine.js
+++ js/engine.js
@@ -171,7 +171,10 @@ var Engine = (function(global) {
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-girl.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-princess-girl.png',
+        'images/char-horn-girl.png'
     ]);
     Resources.onReady(init);
 
