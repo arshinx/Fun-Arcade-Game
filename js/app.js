44e3c550dd0847a649a891706f386e8b2e958ff0
Arshin Jain <arshinx@users.noreply.github.com>
2017-09-30 12:01:46 -0400

    Speed Property for Enemy

--- js/app.js
+++ js/app.js
@@ -29,6 +29,7 @@ var Enemy = function(x, y) {
   // we've provided one for you to get started
   this.x = x; // horizontal movement
   this.y = y; // vertical movement
+  this.speed = 100;
 
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
