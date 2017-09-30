7d33ca031b1854d666d3e8be86d134aef95aabff
Arshin Jain <arshinx@users.noreply.github.com>
2017-09-29 23:25:35 -0400

    Add Movement for Enemies

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ Enemy.prototype.update = function(dt) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
-  this.x *= dt
+  this.x += 100*dt
 };
 
 // Draw the enemy on the screen, required method for game
